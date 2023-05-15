af7b8aa26fe2286a1fb22641849262eda3325038
hirosuke-pi <betacode.net@gmail.com>
2023-05-15 18:51:41 +0900

    add task formのmodal実装

--- app/src/components/organisms/Navigation.tsx
+++ app/src/components/organisms/Navigation.tsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import { Flex, Button } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
+import { FormModal } from "./";
 
 const Navigation = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <Flex width="100%" justifyContent="flex-end" mt={10}>
-      <Button colorScheme="blue">
+      <Button colorScheme="blue" onClick={() => setModalVisible(true)}>
         <AddIcon mr={5} mb={3} /> タスク追加
       </Button>
+      <FormModal isOpen={modalVisible} onClose={() => setModalVisible(false)} />
     </Flex>
   );
 };
--- app/src/components/organisms/index.ts
+++ app/src/components/organisms/index.ts
@@ -2,5 +2,6 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Navigation from "./Navigation";
 import CardList from "./CardList";
+import FormModal from "./FormModal";
 
-export { Header, Footer, Navigation, CardList };
+export { Header, Footer, Navigation, CardList, FormModal };
